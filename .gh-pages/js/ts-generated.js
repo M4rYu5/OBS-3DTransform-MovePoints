43339838022b93a8d265d443b0b08abdf57fde4e
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2022-04-29 20:07:35 +0300

    Initial .gh-pages subtree commit

---
+++ .gh-pages/js/ts-generated.js
@@ -0,0 +1,11 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Jane User";
+greeter(user);
+function greeter3(person) {
+    return "Hello, " + person;
+}
+var user3 = "Jane User";
+greeter3(user3);
+//# sourceMappingURL=ts-generated.js.map
