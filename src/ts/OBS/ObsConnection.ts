0104d1fcde8ab091fa14acf39deea4fbd08ed358
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2022-06-21 15:43:41 +0300

    bug: ObsConnection should disconnect on auth error

--- src/ts/OBS/ObsConnection.ts
+++ src/ts/OBS/ObsConnection.ts
@@ -201,7 +201,10 @@ namespace OBS {
                     this.setObsConnectionResult(ConnectionResult.succeed);
                 }
                 if (data.status == "error") {
-                    this.setObsConnectionResult(ConnectionResult.wrongAuthDetails)
+                    {
+                        this.setObsConnectionResult(ConnectionResult.wrongAuthDetails)
+                        this.disconnect();
+                    }
                 }
             }
         }
