9c5169ea13e3138a525b30e8425697ad56a627a1
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2022-07-23 09:33:28 +0300

    added documentation for DispatchArgs.obj

--- src/ts/OBS/Modules/DispatchArgs.ts
+++ src/ts/OBS/Modules/DispatchArgs.ts
@@ -1,10 +1,14 @@
 namespace OBS.Modules{
     /** encapsulate IModule.dispatch function's arguments */
     export class DispatchArgs{
+        /** Parsed object received from OBS WebSocket */
         obj: any;
         connection: ObsConnection;
 
         constructor();
+        /***
+         * @param obj Parsed object received from OBS WebSocket
+         */
         constructor(obj?: any, connection?: ObsConnection){
             this.obj = obj;
             this.connection = connection;
